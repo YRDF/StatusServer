d00d02d3e2e7ff467ff8d18324cbf4f630da6a4a
YRDF <zzh5653@163.com>
2024-10-23 18:08:08 +0800

    登录验证和状态服务

--- StatusServer.cpp
+++ StatusServer.cpp
@@ -2,19 +2,71 @@
 //
 
 #include <iostream>
+#include <json/json.h>
+#include <json/value.h>
+#include <json/reader.h>
+#include "const.h"
+#include "ConfigMgr.h"
+#include "hiredis.h"
+#include "RedisMgr.h"
+#include "MysqlMgr.h"
+#include "AsioIOServicePool.h"
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <boost/asio.hpp>
+#include "StatusServiceImpl.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void RunServer() {
+	auto& cfg = ConfigMgr::Inst();
+
+	std::string server_address(cfg["StatusServer"]["Host"] + ":" + cfg["StatusServer"]["Port"]);
+	StatusServiceImpl service;
+
+	grpc::ServerBuilder builder;
+	// 监听端口和添加服务
+	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+	builder.RegisterService(&service);
+
+	// 构建并启动gRPC服务器
+	std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+	std::cout << "Server listening on " << server_address << std::endl;
+
+	// 创建Boost.Asio的io_context
+	boost::asio::io_context io_context;
+	// 创建signal_set用于捕获SIGINT
+	boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+
+	// 设置异步等待SIGINT信号
+	signals.async_wait([&server, &io_context](const boost::system::error_code& error, int signal_number) {
+		if (!error) {
+			std::cout << "Shutting down server..." << std::endl;
+			server->Shutdown(); // 优雅地关闭服务器
+			io_context.stop(); // 停止io_context
+		}
+		});
+
+	// 在单独的线程中运行io_context
+	std::thread([&io_context]() { io_context.run(); }).detach();
+
+	// 等待服务器关闭
+	server->Wait();
+
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+int main(int argc, char** argv) {
+	try {
+		RunServer();
+		//RedisMgr::GetInstance()->Close();
+	}
+	catch (std::exception const& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		//RedisMgr::GetInstance()->Close();
+		return EXIT_FAILURE;
+	}
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+	return 0;
+}
+
+
